9b5b998dc9d83ed77ea33a92f916346a06548f12
gregpaton08 <gregpaton08@gmail.com>
2015-12-12 15:27:07 -0500

    Initial Commit

---
+++ LightSwitchUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightSwitchUI
+//
+//  Created by Greg Paton on 12/12/15.
+//  Copyright © 2015 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

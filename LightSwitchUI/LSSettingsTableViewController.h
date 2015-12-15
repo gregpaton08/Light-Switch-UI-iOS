9277027859401d7bb3dcedf608fc3209d8f67de4
gregpaton08 <gregpaton08@gmail.com>
2015-12-14 23:29:46 -0500

    Added code for settings view controller.

---
+++ LightSwitchUI/LSSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LSSettingsTableViewController.h
+//  LightSwitchUI
+//
+//  Created by Greg Paton on 12/14/15.
+//  Copyright © 2015 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSSettingsTableViewController : UITableViewController
+
+@end
